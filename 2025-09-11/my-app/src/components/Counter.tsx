e282ee62f3dd6ca26323843017291f1554476337
Kristjan Põldmets <poldmets@tlu.ee>
2025-09-14 17:20:25 +0300

    feat: add values and separate counter
    Add more value buttons and separate the counter from value buttons for clarity.

--- 2025-09-11/my-app/src/components/Counter.tsx
+++ 2025-09-11/my-app/src/components/Counter.tsx
@@ -20,7 +20,21 @@ function Counter() {
     <>
       <h1>Vite + React + Kristjan</h1>
       <div className="card">
-        <button onClick={() => increaseCounter(10)}>count is {count}</button>
+        <div>count is {count}</div>
+        <div>
+          increase count by:
+          <button onClick={() => increaseCounter(100)}>100</button>
+          <button onClick={() => increaseCounter(50)}>50</button>
+          <button onClick={() => increaseCounter(25)}>25</button>
+          <button onClick={() => increaseCounter(1)}>1</button>
+        </div>
+        <div>
+          decrease count by:
+          <button onClick={() => increaseCounter(-1)}>1</button>
+          <button onClick={() => increaseCounter(-25)}>25</button>
+          <button onClick={() => increaseCounter(-50)}>50</button>
+          <button onClick={() => increaseCounter(-100)}>100</button>
+        </div>
       </div>
     </>
   )
